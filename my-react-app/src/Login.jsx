faa6429f022dddaa199c812e5394255f20be3875
Akhileshsenapati1008 <akhileshsenapati93@gmail.com>
2026-08-16 15:38:33 +0530

    Added login Page skeleton

---
+++ my-react-app/src/Login.jsx
@@ -0,0 +1,14 @@
+export function Login(){
+return (
+    <div id="outerLoginDiv">
+        <h1>Student Login</h1>
+        <form>
+            <label id="usernameLabel">username</label>
+            <input id="usernameInput" placeholder="Enter your username" required ></input>
+            <label id="passwordLabel">Password</label>
+            <input id="passwordInput" type="password" placeholder="Enter your password" required ></input>
+            <button id="loginButton" type="submit">Login</button>
+        </form>
+    </div>
+)
+}
