93c23a09bfcb95c40b4b0a27240afba005b4be8c
Akhileshsenapati1008 <akhileshsenapati93@gmail.com>
2026-08-16 16:45:12 +0530

    add forgot password hyperlink still non-functional(last priotity)

--- my-react-app/src/Login.jsx
+++ my-react-app/src/Login.jsx
@@ -8,6 +8,7 @@ return (
             <label id="passwordLabel">Password</label>
             <input id="passwordInput" type="password" placeholder="Enter your password" required ></input>
             <button id="loginButton" type="submit">Login</button>
+            <a href="/forgot-password">Forgot Password?</a>
         </form>
     </div>
 )
