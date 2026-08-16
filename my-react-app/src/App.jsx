3d91a7e541399a4869abc994d0bcb18a77c7550d
Akhileshsenapati1008 <akhileshsenapati93@gmail.com>
2026-08-16 21:01:30 +0530

    Setting unaccesable to students as of yet

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       {isLoggedIn?null:<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} username={username} setUsername={setUsername}></Login>}
       {(isLoggedIn&&currentPage==="profile")?<Profile username={username} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUsername={setUsername} currentPage={currentPage} setCurrentPage={setCurrentPage}></Profile>:null}
       {(isLoggedIn&&currentPage==="dashboard")?<Dashboard username={username} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUsername={setUsername} currentPage={currentPage} setCurrentPage={setCurrentPage}></Dashboard>:null}
-      {(isLoggedIn&&currentPage==="settings")?<Settings></Settings>:null}
+      {(isLoggedIn&&currentPage==="settings")?<Settings isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} username={username} setUsername={setUsername} currentPage={currentPage} setCurrentPage={setCurrentPage}></Settings>:null}
     </div>
   )
 }
--- my-react-app/src/Settings.jsx
+++ my-react-app/src/Settings.jsx
@@ -1,7 +1,20 @@
-export function Settings(){
+export function Settings({isLoggedIn, setIsLoggedIn, username, setUsername, currentPage, setCurrentPage}){
     return(
         <div className="settings">
             <h1>Welcome to your settings</h1>
+            <p>Here you can manage your account preferences.</p>
+            <p>Unfortunately Locked for all Students</p>
+            <button onClick={()=>{
+                setIsLoggedIn(false);
+                setUsername("");
+                setCurrentPage("login");
+            }}>Logout</button>
+            <button onClick={()=>{
+                setCurrentPage("settings");
+            }}>Settings</button>
+            <button onClick={()=>{
+                setCurrentPage("dashboard");
+            }}>dashboard</button>
         </div>
     )
 }
